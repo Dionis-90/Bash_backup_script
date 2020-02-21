8a511799a687bccb0cb8ddb604d32b229519f24e
Dionis <dionis201290@gmail.com>
2020-02-21 22:22:02 +0200

    Separate settings to conf file. Change readme.

--- README.md
+++ README.md
@@ -12,23 +12,17 @@ or if you need to use SMB
 sudo apt update && sudo apt install mailutils smbclient
 ```
 ## Usage
-1. Put this script to `</root>` 
-2. Open backup.sh on text-editor
-3. Using tips in the script set variables:
-   CLIENT 
-   MYSQLBAK
-   EMAIL (optioanal)
-   DEST
-   FTPHOST or SMBHOST
-if you need to use SMB:
-   SMBUSER
-   SMBPASS
-   SMBRESURS
-4. If you need to use ftp-client put .netrc to `/root` and set correct permissions
+1. Put this script to `/root` 
+2. Open backup.conf on text-editor and using tips set the necessary settings.
+3. If you need to use ftp-client put .netrc to `/root` and set correct permissions
 
    `sudo chown root:root /root/.netrc && sudo chmod 600 /root/.netrc`
    
-5. Open .netrc in text-editor and set correct authentication data
+4. Open .netrc in text-editor and set correct authentication data
+5. Set correct permission to the script
+
+`sudo chmod +x /root/backup.sh`
+
 6. Add cron-task to root-crontab.
 
 `sudo echo "0 2 * * *	root	/root/backup.sh 2>/dev/null" >> /etc/crontab`
--- backup.sh
+++ backup.sh
@@ -1,42 +1,12 @@
 #!/bin/bash
-##Add to crontab example
-# 0 2 * * *	root	/root/backup.sh 2>/dev/null
+source backup.conf
 
-###VARIABLES
-##Choose upload client
-# 1 - use FTP-client (.netrc require for auth.)
-# 2 - SMB-client
-CLIENT='2'
-
-##MYSQL BACKUP
-#1 - Use MySQL backup
-MYSQLBAK='1'
-NONSTARTDSKPERCENT="90"
-
-FTPHOST='192.168.1.3'
-SMBHOST='192.168.1.3'
-SMBUSER='backup'
-SMBPASS='your_SMB-Password'
-SMBRESURS='backup'
-DATE=`date +"%Y-%m-%d"`
-TIME=`date +"%H:%M:%S"`
-EMAIL='root'
-BACKUPLOGPTH="/var/log/backup-${DATE}.log"
-
-#Set your space-separeted pathes which you will need to backup.
-DEST="`ls /home/`"
-#Example
-#DEST="/root/1 /root/2 /etc"
-
-echo -e "$DATE $TIME\n--------------------------------------------------------------------------------------------" >> $BACKUPLOGPTH
+echo -e "$DATE $TIME\n--------------------------------------------------------------------------------------------" >> $BACKUPLOGPTH || echo "Can not to create log-file. Probably you run script with not enough permissions! Exiting..." && exit 1
 
 function error() {
 echo "Backup unsuccessful on `hostname`." | mail -s "Backup unsuccessful" $EMAIL -A $BACKUPLOGPTH
 exit 1
 }
-
-#You will need to use auth_socket plugin for mysql-user root. Or specify parameter -pYour_password for mysql and mysqladmin.
-MYSQLDBS=`mysql -Bse "show databases;" | grep -v '.*_schema' | grep -v 'sys' | grep -v 'test' | xargs`
 
 [ "$CLIENT" -eq "1" ] && which ftp > /dev/null && REMOTE="ftp $FTPHOST" && echo "Using FTP." >> $BACKUPLOGPTH
 [ "$CLIENT" -eq "2" ] && which smbclient > /dev/null && REMOTE="smbclient -U $SMBUSER //$SMBHOST/$SMBRESURS $SMBPASS" && echo "Using SMB." >> $BACKUPLOGPTH
@@ -76,7 +46,6 @@ if [ "$MYSQLBAK" -eq "1" ]
 	else
 	echo "MySQL backup not enabled" >> $BACKUPLOGPTH
 fi
-
 
 [ -z $DEST ] && echo "Folders for backup is not specified. Please see readme." >> $BACKUPLOGPTH && error
 for i in ${DEST}
